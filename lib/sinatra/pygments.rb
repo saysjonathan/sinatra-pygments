295519681c5edeadb36bb7c46f04d5d9d4e3e2d4
Jonathan Boyett <jonathan@failingservers.com>
2011-01-21 13:48:54 -0800

    simplify helper

--- lib/sinatra/pygments.rb
+++ lib/sinatra/pygments.rb
@@ -2,31 +2,13 @@ require 'sinatra/base'
 
 module Sinatra
   module Pygments
-    @@pygments = '/usr/bin/pygmentize'
-    
-    @@options = {
-      :lexer => '-l',
-      :filter => '-F',
-      :format => '-f',
-      :options => '-O'
-    }
-
-    def initialize(string, lexer = :text, format = :html, filter = nil, options = nil)
-      args = []
-      @@options.each do |k, v|
-        args << '#{v} {k}' unless k.nil?
-      end
-      color(string, args)
-    end
-
-    def color(string, *options)
-      html = ''
-      IO.popen(@@pygments + options.join(' ')).each do |p|
-        p.write string
-        p.close_write
-        html = p.read.strip
-      end
-      html
-    end
+    def pygmentize(string, lexer = 'text')
+      pygmentize = IO.popen("pygmentize -f html -l #{lexer}", "w+") 
+      pygmentize.puts string
+      pygmentize.close_write 
+      result = pygmentize.read
+      pygmentize.close
+      result
+    end 
   end
 end
